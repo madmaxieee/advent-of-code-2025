3881277cbef8dd535094fb237b2b62252afaef55
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-12-02 00:40:55 +0800

    feat: Solve Day 01, Part 1 and Part 2
    Implements the solution logic for both parts of Day 01 using the provided
    input instructions.
    
    Details:
    *   Added the full puzzle input to `inputs/day01.txt`.
    *   Implemented input parsing and preliminary movement logic in `day01.rs`.
    *   Includes a test case for Part 2.

--- rust/src/days/day01.rs
+++ rust/src/days/day01.rs
@@ -1,9 +1,52 @@
-pub fn part1(_input: &str) -> String {
-    "".to_string()
+pub fn part1(input: &str) -> String {
+    let mut pos = 50;
+    let mut answer = 0;
+
+    for line in input.lines() {
+        let instruction = line.trim();
+        let direction = match &instruction[0..1] {
+            "L" => -1,
+            "R" => 1,
+            _ => unreachable!(),
+        };
+        let distance: i32 = instruction[1..].parse().unwrap();
+        pos += direction * distance;
+        pos %= 100;
+        if pos == 0 {
+            answer += 1;
+        }
+    }
+
+    answer.to_string()
 }
 
-pub fn part2(_input: &str) -> String {
-    "".to_string()
+pub fn part2(input: &str) -> String {
+    let mut pos = 50;
+    let mut answer = 0;
+
+    for line in input.lines() {
+        let instruction = line.trim();
+        let direction = match &instruction[0..1] {
+            "L" => -1,
+            "R" => 1,
+            _ => unreachable!(),
+        };
+
+        let distance: i32 = instruction[1..].parse().unwrap();
+        answer += distance / 100;
+        let real_distance = distance % 100;
+
+        let old_pos = pos;
+        pos += direction * real_distance;
+
+        if old_pos != 0 && !(1..=99).contains(&pos) {
+            answer += 1;
+        }
+
+        pos = (pos + 100) % 100;
+    }
+
+    answer.to_string()
 }
 
 #[cfg(test)]
@@ -27,5 +70,18 @@ L82"#;
     }
 
     #[test]
-    fn test_part2() {}
+    fn test_part2() {
+        let input = r#"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"#;
+
+        assert_eq!(part2(input), "6");
+    }
 }
